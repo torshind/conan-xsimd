6f65de53071d0c6d4886de1f738dae50b8a9b555
Marcello Mansueto <mvm@mydatamodels.com>
2019-06-12 14:58:18 +0200

    Removed git tools

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,12 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake
 
 
 class XsimdConan(ConanFile):
     name = "xsimd"
     version = "7.2.3"
     license = "BSD 3-Clause"
-    url = "https://github.com/QuantStack/xtl"
+    homepage = "https://github.com/QuantStack/xtl/"
+    url = "https://github.com/torshind/conan-xsimd/"
     description = "Modern, portable C++ wrappers for SIMD intrinsics and parallelized, optimized math implementations" \
                   "(SSE, AVX, NEON, AVX512)"
     settings = "os", "compiler", "build_type", "arch"
@@ -15,17 +16,17 @@ class XsimdConan(ConanFile):
     exports_sources = "*"
 
     def source(self):
-        git = tools.Git()
-        git.clone("https://github.com/QuantStack/xsimd.git", self.version)
+        self.run("git clone --branch "
+                 + self.version
+                 + " https://github.com/QuantStack/xsimd.git")
 
     def build(self):
         pass
 
     def package(self):
         cmake = CMake(self)
-        cmake.configure()
+        cmake.configure(source_folder="xsimd")
         cmake.install()
 
     def package_info(self):
         self.info.header_only()
-        self.cpp_info.libs = ["xsimd"]
